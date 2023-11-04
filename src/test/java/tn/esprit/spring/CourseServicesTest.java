6487956b56703eab9544b1de13cb9e09e90b62d4
Mohamed Amine Koubaa <mohamedamine.koubaa@esprit.tn>
2023-11-04 21:20:53 +0100

    test java service :cource

--- src/test/java/tn/esprit/spring/CourseServicesTest.java
+++ src/test/java/tn/esprit/spring/CourseServicesTest.java
@@ -1,13 +1,51 @@
-package tn.esprit.spring;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.repositories.ICourseRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class GestionStationSkiApplicationTests {
+class CourseServicesTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @InjectMocks
+    CourseServicesImpl courseServices;
 
+    @Mock
+    ICourseRepository courseRepository;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testRetrieveAllCourses() {
+        // Prepare mock data
+        Course course1 = new Course(1L, 1, TypeCourse.COLLECTIVE_CHILDREN, Support.ONLINE, 100.0F, 60, null);
+        Course course2 = new Course(2L, 2, TypeCourse.COLLECTIVE_ADULT, Support.OFFLINE, 150.0F, 90, null);
+        List<Course> courses = Arrays.asList(course1, course2);
+
+        // Configure the behavior of the mock
+        Mockito.when(courseRepository.findAll()).thenReturn(courses);
+
+        // Call the service method
+        List<Course> retrievedCourses = courseServices.retrieveAllCourses();
+
+        // Assertions
+        assertNotNull(retrievedCourses);
+        assertEquals(2, retrievedCourses.size());
+    }
+
+    // Add more test methods for other service methods as needed
 }
